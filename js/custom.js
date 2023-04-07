5dca2408644fe6c15fd475d5acf1801771e3d7fd
Salim Kamali <salim.kamali480@gmail.com>
2023-04-07 16:57:56 +0500

    Add Some Section

--- js/custom.js
+++ js/custom.js
@@ -2,12 +2,22 @@
 $(document).on('ready', function () {
     $(".client-slider").slick({
         dots: false,
-        arrows:true,
+        arrows: true,
         infinite: true,
-        autoplay:true, 
-        speed:1000,
+        autoplay: true,
+        speed: 1000,
         autoplaySpeed: 2000,
         slidesToShow: 1,
+        slidesToScroll: 1
+    });
+     $(".logo-slider").slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        autoplay: true,
+        speed: 1000,
+        autoplaySpeed: 2000,
+        slidesToShow: 5,
         slidesToScroll: 1
     });
 });
